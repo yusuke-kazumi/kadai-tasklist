203966cc3ea0b08193f50055bfae48499c1a6460
yusuke.kazumi <yusuke.a.kazumi@rakuten.com>
2018-06-01 04:39:38 +0000

    add column

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -6,6 +6,9 @@
 
     {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
 
+    	{!! Form::label('title','タイトル:'!!}
+	{!! Form::text('title')!!}
+
         {!! Form::label('content', 'tasklist:') !!}
         {!! Form::text('content') !!}
 
--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -4,11 +4,13 @@
 
     <h1>id = {{ $task->id }} のtask細ページ</h1>
 
-    <p>{{ $task->content }}</p>
+    <p>タイトル:{{ $task->content }}</p>
+    <p>メッセージ:{{$task->content}}</p>
     
     {!! link_to_route('tasks.edit', 'このメッセージを編集', ['id' => $task->id]) !!}
     
     {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
     {!! Form::submit('削除') !!}
+ {!! Form::close()!!}
 
 @endsection
