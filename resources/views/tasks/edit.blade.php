f7a934f3d9969a89f1a2391e4ee61d4cb2355ab8
yusuke.kazumi <yusuke.a.kazumi@rakuten.com>
2018-06-05 03:45:57 +0000

    bootstrap2

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -6,16 +6,16 @@
     <h1>id: {{ $task->id }} のtask編集ページ</h1>
 <div class="row">
     
-    <div class="col-xs-6">
+    <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
     {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
         <div class="form-group">
-    	{!! Form::label('status','id:')!!}
-	    {!! Form::text('status', null,[class=>'form-control'])!!}
+    	{!! Form::label('status','status:')!!}
+	    {!! Form::text('status', null,['class'=>'form-control'])!!}
         </div>
         
         <div class="form-group">
         {!! Form::label('content', 'tasklist:') !!}
-        {!! Form::text('content',null, [class=>'form-control']) !!}
+        {!! Form::text('content',null, ['class'=>'form-control']) !!}
         </div>
         
         {!! Form::submit('更新', ['class'=>'btn btn-default']) !!}
